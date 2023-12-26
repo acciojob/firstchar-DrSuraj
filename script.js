fd8002cfca5d7da4166226f38b39ad00b51fdadf
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 16:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str: string): string {
+    // Trim the input string to remove leading and trailing spaces
+    const trimmedStr = str.trim();
+
+    // Check if the trimmed string is not empty
+    if (trimmedStr.length > 0) {
+        // Return the first character of the trimmed string
+        return trimmedStr.charAt(0);
+    } else {
+        // Return an empty string if the input string is empty or contains only spaces
+        return '';
+    }
 }
 
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// Examples
+console.log(firstChar(' Rosa Parks '));  // Output: 'R'
+console.log(firstChar(' Hello World ')); // Output: 'H'
+console.log(firstChar(' '));              // Output: ''
+console.log(firstChar(''));               // Output: ''
